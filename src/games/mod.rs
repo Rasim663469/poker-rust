db0310212b10e60ba00857ce32121497cee5a461
Tom LEARDI <tom.leardi@etu.umontpellier.fr>
2026-03-11 19:35:17 +0100

    création et initialisation de la roulette

--- src/games/mod.rs
+++ src/games/mod.rs
@@ -1,4 +1,8 @@
 pub mod blackjack;
 pub mod poker;
 pub mod slotmachine;
+<<<<<<< Updated upstream
 pub mod hilo;
+=======
+pub mod roulette;
+>>>>>>> Stashed changes
---
+++ src/games/roulette/mod.rs
@@ -0,0 +1,3 @@
+pub mod engine;
+
+pub use engine::{Roulette, RouletteResult, RouletteColor, EUROPEAN_WHEEL_ORDER, european_color_for_number};
